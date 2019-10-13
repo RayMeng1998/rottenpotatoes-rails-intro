1f636670990ce22efca54a9c7ef912bdb02db08f
Ziyue Meng <z.y.meng@berkeley.edu>
2019-10-13 15:34:00 -0700

    Update movies_controller.rb

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,44 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @all_ratings = Movie.get_ratings
+
+    if params[:sort_by] == "title"
+      session[:sort_by] = "title"
+    elsif params[:sort_by] == "release_date"
+      session[:sort_by] = "date"
+    end
+
+    if session[:sort_by] == "title"
+       @movies = Movie.re_order("title")
+       @sorted_col = "title"
+    elsif session[:sort_by] == "date"
+       @movies = Movie.re_order("release_date")
+       @sorted_col = "date"
+    else
+       @movies = Movie.all
+    end
+
+    # if !(params[:ratings].nil?)
+    #   session[:ratings] = params[:ratings]
+    # end
+    # @checked_keys = Array.new
+    # if session[:ratings]
+    #   @checked_keys = session[:ratings].keys
+    #   @filtered_movie_list = Array.new
+    #   @movies.each do |movie|
+    #     if @checked_keys.include? movie[:rating]
+    #       @filtered_movie_list.push movie
+    #     end
+    #   end
+    #   @movies = @filtered_movie_list
+    # end
+    # if(params[:sort_by] == nil  && params[:ratings] == nil)
+    #   if(session[:sort_by] || session[:ratings])
+    #     redirect_to movies_path(:sort_by => session[:sort_by], :ratings => session[:ratings])
+    #   end
+    # end
+
   end
 
   def new
